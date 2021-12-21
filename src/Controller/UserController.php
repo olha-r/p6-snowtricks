63fe9c49d2e4fe1034f0e39ad6eaf90a38d163d8
Olha Raulet <ole4ka.safonova@gmail.com>
2021-12-21 02:01:40 +0100

    Add registration form

---
+++ src/Controller/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/inscription", name="registration", methods={"GET", "POST"})
+     */
+    public function registration(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setToken('124');
+
+            $hash = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hash);
+
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_login', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('user/registration.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+}
