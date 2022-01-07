2ae80ff6e4f6a46c779f04f1956bf667f9190032
Olha Raulet <ole4ka.safonova@gmail.com>
2022-01-07 23:03:36 +0100

    Add slug

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -68,6 +68,11 @@ class Trick
      */
     private $media;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $slug;
+
     public function __construct()
     {
         $this->comments = new ArrayCollection();
@@ -207,6 +212,18 @@ class Trick
                 $media->setTrick(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }
