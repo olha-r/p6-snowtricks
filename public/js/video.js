63c4e0eb1f09cde028ef9c76f1fafebb81ac0256
Olha Raulet <ole4ka.safonova@gmail.com>
2022-02-17 10:07:27 +0100

    Change links to embed links (from youtube and dailymotion) via js

--- public/js/video.js
+++ public/js/video.js
@@ -1,12 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
-    $('#trick_videos').on('change', function() {
-        let preview = $('#video-preview');
-        console.log(preview)
-        if(preview.length) {
-            preview.attr('src', $(this).val());
+    $('#trick_videos').on('change', function () {
+        let url = document.getElementById('trick_videos').value;
+
+        // console.log(url);
+        // console.log(isValidURL(url));
+        if (isValidURL(url) === true) {
+
+            if (url.includes('embed/')) {
+                addVideoPreview(url);
+            } else if (url.includes('watch?v=')) {
+                let embedUrl = createYoutubeEmbedLink(url);
+                console.log(embedUrl);
+                addVideoPreview(embedUrl);
+            } else if (url.includes('dailymotion.com/video/')){
+                let embedUrl = createDailymotionEmbedLink(url);
+                console.log(embedUrl);
+                addVideoPreview(embedUrl);
+            }
         } else {
-            console.log(this)
-            $(this).after($('<iframe id="video-preview" width="300" height="200" src="' + $(this).val() + '" />'));
+            alert('Saisissez correcte l\'URL')
         }
     });
 })
+
+function isValidURL(str) {
+    let pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    return !!pattern.test(str);
+}
+
+function createYoutubeEmbedLink(link) {
+    let splitLink = link.split('watch?v=');
+    return splitLink.join("embed/");
+}
+
+function addVideoPreview(link) {
+    let preview = $('#video-preview');
+    if (preview.length) {
+        preview.attr('src', $(this).val());
+    } else {
+        let videoPreview = $(' <iframe id="video-preview" width="300" height="200" src="' + link + '" />');
+        videoPreview.appendTo('.trick-videos');
+    }
+}
+
+function createDailymotionEmbedLink(link) {
+    let splitLink = link.split('dailymotion.com/video/');
+    return splitLink.join("dailymotion.com/embed/video/");
+}
